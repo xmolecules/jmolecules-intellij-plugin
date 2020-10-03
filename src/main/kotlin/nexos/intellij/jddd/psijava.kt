db5ae1ad1974f6b80282019ea77a21e126d35c59
nexos <nexoscp@users.noreply.github.com>
2020-10-04 01:21:57 +0200

    cleanup

--- src/main/kotlin/nexos/intellij/jddd/psijava.kt
+++ src/main/kotlin/nexos/intellij/jddd/psijava.kt
@@ -13,21 +13,17 @@ private fun findPackageAnnotations(psiFile: PsiJavaFile, annotationsByFQName: Ma
                 ?: listOf()
 
 private fun findTopLevelClassAnnotations(psiFile: PsiJavaFile, annotationsByFQName: Map<String, Info>)
-        = psiFile.classes.map { clazz ->
-    clazz.annotations.mapNotNull { annotationsByFQName[it.qualifiedName] }
-}.flatten()
+        = psiFile.classes.map { it.annotations.mapNotNull { annotationsByFQName[it.qualifiedName] }}.flatten()
 
 private class Cached(private val file: PsiJavaFile): CachedValueProvider<List<Info>> {
     companion object {
         val annotationsByFQName: Map<String, Info> by lazy { Info.all.associateBy { it.fqName }}
     }
 
-    override fun compute(): Result<List<Info>>? {
-        return Result(
+    override fun compute() = Result(
                 findPackageAnnotations(file, annotationsByFQName)
                         + findTopLevelClassAnnotations(file, annotationsByFQName)
                 , file)
-    }
 }
 
 fun cached(file: PsiJavaFile): List<Info> = CachedValuesManager.getCachedValue(file, Cached(file))
